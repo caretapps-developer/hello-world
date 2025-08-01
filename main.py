009eb238ac417492ee0955b6203f758b5839be8b
ammemonaws <aamemonaws@gmail.com>
2025-08-01 18:16:55 +0530

    feat: init commit 01

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Asak, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Bot Asak, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
